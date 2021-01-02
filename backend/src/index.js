ea7ce2aa4f99935cc6ccb44cb2ec50a7727d26c1
Damaso Junio <junio.pereirab@gmail.com>
2021-01-02 16:19:33 -0300

    Configuring http and web socket

--- backend/src/index.js
+++ backend/src/index.js
@@ -2,16 +2,28 @@ const express = require('express');
 const mongoose = require('mongoose');
 const routes = require('./routes');
 const path = require('path');
+const cors = require('cors');
 
 const app = express();
+
+const server = require('http').Server(app);
+const io = require('socket.io')(server);
 
 mongoose.connect('mongodb+srv://juniopereira:junio123@cluster0.ygj9i.mongodb.net/<dbname>?retryWrites=true&w=majority', {
     useNewUrlParser: true,
     useUnifiedTopology: true
-})
+});
+
+app.use((req, res, next) => {
+    req.io = io;
+
+    next();
+});
+
+app.use(cors());
 
 app.use('/files', express.static(path.resolve(__dirname, '..', 'uploads', 'resized')));
 
 app.use(routes);
 
-app.listen(3000);
+server.listen(3000);
