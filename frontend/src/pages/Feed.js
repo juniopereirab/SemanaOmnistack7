607b5bb0053670b72296ab5e82ce56d99f092343
Damaso Junio <junio.pereirab@gmail.com>
2021-01-02 18:12:10 -0300

    Finishing connection between frontend and backend

--- frontend/src/pages/Feed.js
+++ frontend/src/pages/Feed.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import api from '../services/api';
 
 import './Feed.css';
 import more from '../assets/more.svg';
@@ -6,60 +7,49 @@ import like from '../assets/like.svg';
 import comment from '../assets/comment.svg';
 import send from '../assets/send.svg';
 
+
 class Feed extends Component {
+
+    state = {
+        feed: [],
+    };
+
+    async componentDidMount() {
+        const response = await api.get('posts');
+        this.setState({feed: response.data});
+    }
+
+
     render(){
         return(
             <section id="post-list">
-                <article>
-                    <header>
-                        <div className="user-info">
-                            <span>Junio Pereira</span>
-                            <span className="place">Brasilia</span>
-                        </div>
-                        <img src={more} alt="Mais" />
-                    </header>
+                {this.state.feed.map(post => (
+                    <article>
+                        <header>
+                            <div className="user-info">
+                                <span>{post.author}</span>
+                                <span className="place">{post.place}</span>
+                            </div>
+                            <img src={more} alt="Mais" />
+                        </header>
 
-                    <img src="http://localhost:3001/files/photo.jpg" alt=""/>
+                        <img src={`http://localhost:3001/files/${post.image}`} alt=""/>
 
-                    <footer>
-                        <div className="actions">
-                            <img src={like} alt="Like"/>
-                            <img src={comment} alt="Comment"/>
-                            <img src={send} alt="Send"/>
-                        </div>
+                        <footer>
+                            <div className="actions">
+                                <img src={like} alt="Like"/>
+                                <img src={comment} alt="Comment"/>
+                                <img src={send} alt="Send"/>
+                            </div>
 
-                        <strong>900 curtidas</strong>
-                        <p>
-                            Post massa
-                            <span>#Isso #Aquilo</span>
-                        </p>
-                    </footer>
-                </article>
-                <article>
-                    <header>
-                        <div className="user-info">
-                            <span>Junio Pereira</span>
-                            <span className="place">Brasilia</span>
-                        </div>
-                        <img src={more} alt="Mais" />
-                    </header>
-
-                    <img src="http://localhost:3001/files/photo.jpg" alt=""/>
-
-                    <footer>
-                        <div className="actions">
-                            <img src={like} alt="Like"/>
-                            <img src={comment} alt="Comment"/>
-                            <img src={send} alt="Send"/>
-                        </div>
-
-                        <strong>900 curtidas</strong>
-                        <p>
-                            Post massa
-                            <span>#Isso #Aquilo</span>
-                        </p>
-                    </footer>
-                </article>
+                            <strong>{post.likes} curtidas</strong>
+                            <p>
+                                {post.description}
+                                <span>{post.hashtags}</span>
+                            </p>
+                        </footer>
+                    </article>
+                ))}
             </section>
         );
     }
