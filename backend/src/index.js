ba25a6b6ad970f19f30a2b94583ff92b0c788d3a
Damaso Junio <junio.pereirab@gmail.com>
2021-01-02 21:34:21 -0300

    Configuring eslint for backend

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,6 +1,5 @@
 const express = require('express');
 const mongoose = require('mongoose');
-const routes = require('./routes');
 const path = require('path');
 const cors = require('cors');
 
@@ -9,15 +8,17 @@ const app = express();
 const server = require('http').Server(app);
 const io = require('socket.io')(server);
 
+const routes = require('./routes');
+
 mongoose.connect('mongodb+srv://juniopereira:junio123@cluster0.ygj9i.mongodb.net/<dbname>?retryWrites=true&w=majority', {
-    useNewUrlParser: true,
-    useUnifiedTopology: true
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
 });
 
 app.use((req, res, next) => {
-    req.io = io;
+  req.io = io;
 
-    next();
+  next();
 });
 
 app.use(cors());
